2f4b7beb1a84bb3400a8ac4952006a906bdde68e
Ernesto Celis <ernesto@celisdelafuente.net>
2020-08-05 17:39:50 -0500

    Switch computer

--- examples/getall.js
+++ examples/getall.js
@@ -1,12 +1,12 @@
 const zc = require('../index');
 
 (async () => {
-  const records = await zc.readCsv('/home/ecelis/Downloads/HS_18_03_20.csv');
+  const records = await zc.readCsv(process.env.DATA_SOURCE);
   const total = records.length;
   let counter = 0;
   console.log(`Total: ${total}`);
   // TODO Filter unique
-  const addresses = records.map(async record => {
+  const addresses = await records.map(async record => {
     // Calle, Colonia, Ciudad, Estado, CP
     // let address = `${record[29]}, ${record[30]}, ${record[31]}, ${record[32]}, C.P. `;
     let address = `${record[32]}, C.P. `;
@@ -27,12 +27,16 @@ const zc = require('../index');
         address += `${record[33]}`;
     }
     // GeoCode Address
-    //const data = await zc.geoCode(address);
+    await zc.geoCode(encodeURIComponent(address))
+      .then(data => {
     let geoLocation = {
-      zipCode: record[33], coordinates: null //data
+      zipCode: record[33], coordinates: data,
+      localty: record[32]
     }
     counter += 1;
     console.log(`Done: ${geoLocation.zipCode} ${counter} / ${total}`);
     return geoLocation;
   });
+  console.log(addresses);
+  zc.save(addresses);
 })();
--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const os = require("os");
 const fs = require("fs");
 const parse = require("csv-parse/lib/sync");
 const got = require("got");
-
+const MongoClient = require('mongodb').MongoClient;
 if (process.env.NODE_ENV == 'development') {
   require('dotenv').config();
 }
+
+const { MONGODB_PASSWORD, MONGODB_USER, MONGODB_DB } = process.env
+const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.9f0eo.mongodb.net/${MONGODB_DB}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
 const url = "https://geocoder.ls.hereapi.com/search/6.2/geocode.json";
 const apiKey = process.env.HERE_API_KEY;
 
@@ -22,8 +26,8 @@ exports.geoCode = async function geoCode (qry) {
     let result = JSON.parse(res.body);
     let latLon = result.Response.View[0].Result[0].Location.DisplayPosition;
     return latLon;
-  } catch (err) {
-    console.log(`ERROR: ${err.message}`);
+  } catch (e) {
+    console.log(`ERROR: ${e.message}`);
   }
 }
 
@@ -32,4 +36,21 @@ exports.readCsv = async function readCsv (filePath) {
   const content = fs.readFileSync(filePath, "utf8",);
   records = await parse(content);
   return records;
+}
+
+exports.save = function save(doc) {
+  try {
+    client.connect(async err => {
+      const collection = client.db(MONGODB_DB).collection("ZipCodes");
+      let result;
+      if (Array.isArray(doc)) {
+        result = await collection.insertMany(doc)
+      } else {
+        result = await collection.insertOne(doc)
+      }
+      client.close();
+    });
+  } catch (e) {
+    console.error(e);
+  }
 }
